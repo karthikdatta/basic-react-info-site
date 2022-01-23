98a311745317f1ab29c7e621489aaf2626832df6
karthikdatta <karthikdatta669@gmail.com>
2022-01-23 22:22:45 +0530

    2 files modified

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,17 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 export default function Header() {
   return (
-      <header>
-          <nav className="nav">
-              <img className="nav-logo" src="./react-logo.png"></img>
-              <ul className="nav-items">
-                  <li>Pricing</li>
-                  <li>About</li>
-                  <li>Contact</li>
-              </ul>
-          </nav>
-      </header>
-  )
+    <header>
+      <nav className="nav">
+        <img className="nav-logo" src="./react-logo.png"></img>
+        <ul className="nav-items">
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
